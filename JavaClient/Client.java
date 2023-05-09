d18df526ca3ab5eb4e7254892f67d0a382b756d0
murphyvica <murphyv@uw.edu>
2023-05-08 20:54:18 -0700

    Added Client and Server files

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -16,15 +16,16 @@ public class Client {
 
     public static void main(String... args) throws Exception {
         System.out.println(add() == 0);
-        System.out.println(add(1, 2, 3, 4, 5) == 15);
+        /* System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
         System.out.println(subtract(12, 6) == 6);
         System.out.println(multiply(3, 4) == 12);
         System.out.println(multiply(1, 2, 3, 4, 5) == 120);
         System.out.println(divide(10, 5) == 2);
-        System.out.println(modulo(10, 5) == 0);
+        System.out.println(modulo(10, 5) == 0); */
     }
     public static int add(int lhs, int rhs) throws Exception {
+        Request ("add",  new Object[]{lhs, rhs});
         return -1;
     }
     public static int add(Integer... params) throws Exception {
@@ -43,6 +44,40 @@ public class Client {
         return -1;
     }
     public static int modulo(int lhs, int rhs) throws Exception {
+        return -1;
+    }
+
+    public static int Request (String name, Object... argss) {
+
+        String req = "<?xml version=\"1.0\"?>" + "<methodCall>" + "<methodName>" + name + "</methodName>" + "<params>";
+
+        for (Object p : argss) {
+            req += "<param>" + "<value>" + "<i4>" + p + "</i4>" + "</value>" + "</param>";
+        }
+
+        req += "</params>" + "</methodCall>";
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        try { HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/RPC"))
+                .POST(HttpRequest.BodyPublishers.ofString(req))
+                .build();
+        
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return -1;
     }
 }
--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -1,7 +1,13 @@
 package edu.uw.info314.xmlrpc.server;
 
+import java.io.*;
 import java.util.*;
 import java.util.logging.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
 
 import static spark.Spark.*;
 
@@ -14,11 +20,72 @@ public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
 
     public static void main(String[] args) {
-        LOG.info("Starting up on port 4567");
+        port(8080);
 
         // This is the mapping for POST requests to "/RPC";
         // this is where you will want to handle incoming XML-RPC requests
-        post("/RPC", (request, response) -> { response.status(500); return "TBD"; });
+        post("/RPC", (request, response) -> {  
+            String body = request.body();
+            
+            try {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+
+                Document doc = builder.parse(new InputSource(new StringReader(body)));
+
+                XPathFactory xpathFactory = XPathFactory.newInstance();
+                XPath xpath = xpathFactory.newXPath();
+
+                String expr = "/methodCall";
+                NodeList nodeList = (NodeList) xpath.compile(expr).evaluate(doc, XPathConstants.NODESET);
+
+                String methodName = "";
+                int [] params = new int[nodeList.getLength()];
+
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Node n = nodeList.item(i);
+                    Element e = (Element) n;
+                    methodName = (e.getElementsByTagName("methodName").item(0).getTextContent());
+                    params[i] = Integer.parseInt(e.getElementsByTagName("param").item(0).getTextContent());
+                }
+
+                int res = -1;
+
+                Calc c = new Calc();
+
+                if (methodName.equals("add")) {
+                    res = c.add(params);
+
+                } else if (methodName.equals("subtract")) {
+                    int l = (Integer) params[0];
+                    int r = (Integer) params[1];
+                    res = c.subtract(l, r);
+                    
+                } else if (methodName.equals("multiply")) {
+                    res =  c.multiply(params);
+                    
+                } else if (methodName.equals("divide")) {
+                    int l = (Integer) params[0];
+                    int r = (Integer) params[1];
+                    res = c.divide(l, r);
+                    
+                } else if (methodName.equals("modulo")) {
+                    int l = (Integer) params[0];
+                    int r = (Integer) params[1];
+                    res = c.modulo(l, r);
+                }
+                
+                String r = "<?xml version=\"1.0\"?>" + "<response>" + res +  "</response>";
+                response.type("text/xml");
+                return r;
+                
+
+            } catch (Exception e) {
+                response.status(401);
+                return ""; 
+
+            }
+        });
 
         // Each of the verbs has a similar format: get() for GET,
         // put() for PUT, delete() for DELETE. There's also an exception()
