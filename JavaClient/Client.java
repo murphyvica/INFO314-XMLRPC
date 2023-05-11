f53f953cd52ce5cca1efc70494fb33b513ff6dae
murphyvica <murphyv@uw.edu>
2023-05-10 23:57:38 -0700

    added files

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -5,7 +5,10 @@ import javax.xml.parsers.*;
 import javax.xml.xpath.*;
 import org.w3c.dom.*;
 
-import org.w3c.dom.Node;
+import java.util.*;
+import java.util.logging.*;
+
+import org.xml.sax.*;
 
 /**
  * This approach uses the java.net.http.HttpClient classes, which
@@ -13,41 +16,52 @@ import org.w3c.dom.Node;
  */
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    public static String address;
 
     public static void main(String... args) throws Exception {
-        System.out.println(add() == 0);
-        /* System.out.println(add(1, 2, 3, 4, 5) == 15);
+        address = "http://" + args[0] + ":" + args[1] + "/RPC";
+
+        System.out.println(add(0) == 0);
+        System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
         System.out.println(subtract(12, 6) == 6);
         System.out.println(multiply(3, 4) == 12);
         System.out.println(multiply(1, 2, 3, 4, 5) == 120);
         System.out.println(divide(10, 5) == 2);
-        System.out.println(modulo(10, 5) == 0); */
+        System.out.println(modulo(10, 5) == 0);
     }
     public static int add(int lhs, int rhs) throws Exception {
-        Request ("add",  new Object[]{lhs, rhs});
-        return -1;
+        int res = Request ("add",  new Integer[]{lhs, rhs});
+        return res;
     }
     public static int add(Integer... params) throws Exception {
-        return -1;
+        int res = Request ("add",  params);
+        return res;
     }
     public static int subtract(int lhs, int rhs) throws Exception {
-        return -1;
+        int res = Request ("subtract",  new Integer[]{lhs, rhs});
+        return res;
     }
     public static int multiply(int lhs, int rhs) throws Exception {
-        return -1;
+        int res = Request ("multiply",  new Integer[]{lhs, rhs});
+        return res;
     }
     public static int multiply(Integer... params) throws Exception {
-        return -1;
+        int res = Request ("multiply",  params);
+        return res;
     }
     public static int divide(int lhs, int rhs) throws Exception {
-        return -1;
+        int res = Request ("divide",  new Integer[]{lhs, rhs});
+        return res;
     }
     public static int modulo(int lhs, int rhs) throws Exception {
-        return -1;
+        int res = Request ("modulo",  new Integer[]{lhs, rhs});
+        return res;
     }
 
-    public static int Request (String name, Object... argss) {
+    public static int Request (String name, Integer... argss) {
+
+        // Create and send request
 
         String req = "<?xml version=\"1.0\"?>" + "<methodCall>" + "<methodName>" + name + "</methodName>" + "<params>";
 
@@ -57,27 +71,80 @@ public class Client {
 
         req += "</params>" + "</methodCall>";
 
-
         HttpClient client = HttpClient.newHttpClient();
         try { HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/RPC"))
+                .uri(URI.create(address))
                 .POST(HttpRequest.BodyPublishers.ofString(req))
                 .build();
         
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String body = response.body();
 
-        
+        // Parsing Response Body
 
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document doc = builder.parse(new InputSource(new StringReader(body)));
+
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+
+        String test = "/methodResponse/fault";
+        NodeList nodeList0 = (NodeList) xpath.compile(test).evaluate(doc, XPathConstants.NODESET);
+
+        check(nodeList0, doc, xpath);
+
+            String expr = "/methodResponse";
+            NodeList nodeList = (NodeList) xpath.compile(expr).evaluate(doc, XPathConstants.NODESET);
+
+            int param = -1;
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node n = nodeList.item(i);
+                Element e = (Element) n;
+                param = Integer.parseInt(e.getElementsByTagName("param").item(0).getTextContent());
+            }
+
+            return param;
 
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-
+        } catch (ParserConfigurationException p) {
+            p.printStackTrace();
+        } catch (SAXException s) {
+            s.printStackTrace();
+        } catch (XPathException x) {
+            x.printStackTrace();
+        } 
         return -1;
     }
+
+
+    public static void check(NodeList nodeList0, Document doc, XPath xpath) {
+        try {
+            if (nodeList0.getLength() != 0) {
+                String test = "/methodResponse/fault/value/struct/member";
+                NodeList nodeListF = (NodeList) xpath.compile(test).evaluate(doc, XPathConstants.NODESET);
+                int value = -1;
+            
+                Node n = nodeListF.item(0);
+                Element e = (Element) n;
+                value = Integer.parseInt(e.getElementsByTagName("value").item(0).getTextContent());
+
+                if (value == 3) {
+                    throw new IllegalArgumentException();
+
+                } else if (value == 1) {
+                    throw new ArithmeticException();
+                }
+    }
+        } catch (XPathExpressionException x) {
+            x.printStackTrace();
+}
+}
 }
--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -20,7 +20,7 @@ public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
 
     public static void main(String[] args) {
-        port(8080);
+        port(80);
 
         // This is the mapping for POST requests to "/RPC";
         // this is where you will want to handle incoming XML-RPC requests
@@ -36,22 +36,43 @@ public class App {
                 XPathFactory xpathFactory = XPathFactory.newInstance();
                 XPath xpath = xpathFactory.newXPath();
 
+                // Parsing the method:
+
                 String expr = "/methodCall";
                 NodeList nodeList = (NodeList) xpath.compile(expr).evaluate(doc, XPathConstants.NODESET);
 
-                String methodName = "";
-                int [] params = new int[nodeList.getLength()];
+                Node n = nodeList.item(0);
+                Element e = (Element) n;
+                String methodName = (e.getElementsByTagName("methodName").item(0).getTextContent());
 
-                for (int i = 0; i < nodeList.getLength(); i++) {
-                    Node n = nodeList.item(i);
-                    Element e = (Element) n;
-                    methodName = (e.getElementsByTagName("methodName").item(0).getTextContent());
-                    params[i] = Integer.parseInt(e.getElementsByTagName("param").item(0).getTextContent());
+                // Parsing parameters:
+
+                String expr2 = "/methodCall/params/param";
+                NodeList nodeList2 = (NodeList) xpath.compile(expr2).evaluate(doc, XPathConstants.NODESET);
+
+                int [] params = new int[nodeList2.getLength()];
+
+                for (int i = 0; i < nodeList2.getLength(); i++) {
+                Node n2 = nodeList2.item(i);
+                Element e2 = (Element) n2;
+                    params[i] = Integer.parseInt(e2.getElementsByTagName("i4").item(0).getTextContent());
                 }
+
+
+                // Doing calculations:
+
+                int faultCode = 0;
+
+                String faultString = "";
 
                 int res = -1;
 
                 Calc c = new Calc();
+
+                if (params.length == 0) {
+                    faultCode = 3;
+                    faultString = "illegal arguement type" ;
+                }
 
                 if (methodName.equals("add")) {
                     res = c.add(params);
@@ -67,15 +88,38 @@ public class App {
                 } else if (methodName.equals("divide")) {
                     int l = (Integer) params[0];
                     int r = (Integer) params[1];
-                    res = c.divide(l, r);
+                    if (r == 0) {
+                        faultCode = 1;
+                        faultString = "divide by zero";
+                    } else {
+                        res = c.divide(l, r);
+                    }
                     
                 } else if (methodName.equals("modulo")) {
                     int l = (Integer) params[0];
                     int r = (Integer) params[1];
+                    if (r == 0) {
+                        faultCode = 1;
+                        faultString = "divide by zero";
+                    } else {
                     res = c.modulo(l, r);
+                    }
                 }
+
+                String r = "";
                 
-                String r = "<?xml version=\"1.0\"?>" + "<response>" + res +  "</response>";
+                if (faultCode == 0) {
+
+                    r = "<?xml version=\"1.0\"?>" + "<methodResponse>" + "<params>" + "<param>" + "<value>" + "<i4>" +
+                                res + "</i4>" + "</value>" + "</param>" + "</params>" + "</methodResponse>";
+
+                } else {
+                    r = "<?xml version=\"1.0\"?>" + "<methodResponse>" +  "<fault>" + "<value>" + "<struct>" + "<member>" + "<name>" + 
+                                "faultCode" + "</name>" + "<value>" + "<int>" + faultCode + "</int>" + "</value>" + "</member>" +
+                                "<member>" + "<name>" + "faultString" + "</name>" + "<value>" + "<string>" + faultString + "</string>" + 
+                                "</value>" + "</member>" + "</struct>" + "</value>" + "</fault>" + "</methodResponse>";
+                }
+
                 response.type("text/xml");
                 return r;
                 
